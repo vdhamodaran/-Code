d8fca43db10a6372e6189e9ceef0b911797434e3
vdhamoda <Ultron@Vigneshs-MacBook-Pro.local>
2016-02-17 15:39:11 -0500

    updated functions in custom linked list
    added functions for adding, retrieving and deleting first element in
    the linked list

--- DataStructures and Algorithms/Lists/LinkedList/linkedList.java
+++ DataStructures and Algorithms/Lists/LinkedList/linkedList.java
@@ -2,17 +2,55 @@ import java.io.*;
 import java.util.*;
 
 class linkedList<AnyType> implements Iterable<AnyType>{
+  //defining head the pointer for first element of the linkedList
   private Node<AnyType> head;
+
+  //Creating an Empty linkedList
   public linkedList(){
     head = null;
   }
 
+  //Checking wheather the linkedList is empty
   public boolean isEmpty(){
+    //returns list is empty if the head value = null
     if(head==null){
       return true;
     }
     return false;
   }
+
+  //Adding an element at the begining of a linkedList
+  public void addFirst(AnyType item){
+    head = new Node(item,head);
+  }
+
+  //get the first element of the linkedList
+  public void getFirst(){
+    Node temp = head;
+    if(head!= null){
+      System.out.println("First Data: "+temp.data+" Next: "+temp.next);
+    }
+    else{
+      throw new NoSuchElementException();
+    }
+  }
+
+  //
+  public void deleteFirst(){
+    System.out.println("DeletedFirst Data: "+head.data+" Next: "+head.next);
+    head = head.next;
+  }
+
+  //traverse and print list
+  public void traverse(){
+    Node temp = head;
+    System.out.println("List Contents");
+    while(temp!= null){
+      System.out.println("Data: "+temp.data+" Next: "+temp.next);
+      temp = temp.next;
+    }
+  }
+
 //Node is an inner class which is a represntaion of node/block in a linkedList
   private static class Node<AnyType>{
     private AnyType data;
@@ -43,6 +81,17 @@ class linkedList<AnyType> implements Iterable<AnyType>{
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
 
+   }
+   public static void main(String[] args) {
+     linkedList<String> list = new linkedList <String>();
+     list.addFirst("p");
+     list.addFirst("a");
+     list.addFirst("e");
+     list.addFirst("h");
+     list.traverse();
+     list.getFirst();
+     list.deleteFirst();
+     list.traverse();
    }
 
 }
